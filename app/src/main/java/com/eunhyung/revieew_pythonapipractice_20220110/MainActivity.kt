c425e0c3af7a79d558dbf6097d3ee14eec03f648
전은형 <ooooo5h91@gmail.com>
2022-01-10 21:59:02 +0900

    실제 로그인 기능 호출

--- app/src/main/java/com/eunhyung/revieew_pythonapipractice_20220110/MainActivity.kt
+++ app/src/main/java/com/eunhyung/revieew_pythonapipractice_20220110/MainActivity.kt
@@ -2,7 +2,11 @@ package com.eunhyung.revieew_pythonapipractice_20220110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.revieew_pythonapipractice_20220110.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +14,24 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
 
         btnLogin.setOnClickListener {
+
+            val inputId = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+            apiList.getRequestLogin(inputId, inputPw).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+
+            })
 
         }
     }
