399581fd829a91f1e7df8129e3ff2923e4886ea7
전은형 <ooooo5h91@gmail.com>
2022-01-10 22:12:10 +0900

    회원가입 API 실행 준비

---
+++ app/src/main/java/com/eunhyung/revieew_pythonapipractice_20220110/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.eunhyung.revieew_pythonapipractice_20220110
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        btnSubmit.setOnClickListener {
+
+
+        }
+    }
+}
